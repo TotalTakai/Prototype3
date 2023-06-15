bc41ce8a1830fff6979cf3fd2519c74c9e626209
Nir <yanaynir1@gmail.com>
2023-06-15 14:01:40 +0300

    Adding Features
    Adding Features
    
    1. Added explosion death particle
    2. Added dirt running particle
    3. Added background music
    4. Added jump sound-effects
    
    Completed the project pre-challanges

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,8 +6,14 @@ public class PlayerController : MonoBehaviour
 {
     private Rigidbody rigidRb;
     private Animator playerAnim;
+    private AudioSource playerAudio;
     private float jumpForce = 700.0f;
     private bool isOnGround = true;
+
+    public ParticleSystem explosionParticle;
+    public ParticleSystem dirtParticle;
+    public AudioClip jumpSound;
+    public AudioClip crashSound;
     public float gravityModifier = 1.0f;
     public bool gameOver = false;
 
@@ -16,6 +22,7 @@ public class PlayerController : MonoBehaviour
     {
         rigidRb = GetComponent<Rigidbody>();
         playerAnim = GetComponent<Animator>();
+        playerAudio = GetComponent<AudioSource>();
         Physics.gravity *= gravityModifier;
     }
 
@@ -33,6 +40,8 @@ public class PlayerController : MonoBehaviour
             rigidRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             playerAnim.SetTrigger("Jump_trig");
             isOnGround = false;
+            dirtParticle.Stop();
+            playerAudio.PlayOneShot(jumpSound);
         }
     }
 
@@ -42,12 +51,22 @@ public class PlayerController : MonoBehaviour
         if (collision.gameObject.CompareTag("Ground"))
         {
             isOnGround = true;
+            if (!gameOver)
+            {
+                dirtParticle.Play();
+            }
         } else if (collision.gameObject.CompareTag("Obstacle"))
         {
+            if (!gameOver)
+            {
+                explosionParticle.Play();
+                playerAudio.PlayOneShot(crashSound);
+            }
             gameOver = true;
             Debug.Log("Game Over");
             playerAnim.SetBool("Death_b", true);
             playerAnim.SetInteger("DeathType_int", 1);
+            dirtParticle.Stop();
         }
         
     }
